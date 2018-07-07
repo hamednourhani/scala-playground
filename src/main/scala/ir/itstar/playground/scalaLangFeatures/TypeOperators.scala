c2ffd3281bc1b1ef0e6d9b4b44e2812d424884da
hamednourhani <itstarir@gmail.com>
2018-07-07 18:27:34 +0430

    Add scalaLangFeatures package

---
+++ src/main/scala/ir/itstar/playground/scalaLangFeatures/TypeOperators.scala
@@ -0,0 +1,25 @@
+package ir.itstar.playground.scalaLangFeatures
+
+object TypeOperators extends App {
+
+  trait Parent[T] {
+    val value: T
+
+    case class Child(name: T)
+
+  }
+
+
+  val parent: Parent[String] = new Parent[String] {
+    override val value: String = "parent"
+  }
+
+  val child: Parent[Int]#Child = new Parent[Int] {
+    override val value: Int = 3
+  }.Child(2)
+
+  println(parent)
+  println(child)
+
+
+}
