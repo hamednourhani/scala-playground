78dd6c448056fb31fbc6f184a640984bfee2490b
Hamed Nourhani <itstarir@gmail.com>
2019-01-09 00:19:44 +0330

    HandleWebSocketDiscounnection Added

--- src/main/scala/ir/itstar/playground/akka/stream/HandleWebSocketDisconnection.scala
+++ src/main/scala/ir/itstar/playground/akka/stream/HandleWebSocketDisconnection.scala
@@ -1,25 +1,22 @@
 package ir.itstar.playground.akka.stream
 
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.TextMessage
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import akka.stream.{ActorMaterializer, KillSwitches, UniqueKillSwitch}
-import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.http.scaladsl.server.Route
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, KillSwitches}
 import com.typesafe.scalalogging.LazyLogging
-import ir.itstar.playground.akka.RestMock.routes
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-object HandleWebSocketDisconnection extends App with LazyLogging{
+object HandleWebSocketDisconnection extends App with LazyLogging {
 
   implicit val system: ActorSystem = ActorSystem("HandleWebSocketDisconnection")
-  implicit val ec :ExecutionContext = system.dispatcher
+  implicit val ec: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-
 
 
   private val wsReader: Route =
@@ -30,14 +27,14 @@ object HandleWebSocketDisconnection extends App with LazyLogging{
 
       val testSource =
         Source
-        .repeat("Hello")
-        .throttle(1, 1.seconds)
-        .map(x => {
-        println(x)
-        x
-      })
-      .map(TextMessage.Strict)
-        .limit(1000)
+          .repeat("Hello")
+          .throttle(1, 1.seconds)
+          .map(x => {
+            println(x)
+            x
+          })
+          .map(TextMessage.Strict)
+          .limit(1000)
           .via(sharedKillSwitch.flow)
 
       extractUpgradeToWebSocket { upgrade ⇒
@@ -58,35 +55,34 @@ object HandleWebSocketDisconnection extends App with LazyLogging{
 }
 
 
-object HandleWebSocketDisconnectionTest extends App with LazyLogging{
+object HandleWebSocketDisconnectionTest extends App with LazyLogging {
 
   implicit val system: ActorSystem = ActorSystem("HandleWebSocketDisconnection")
-  implicit val ec :ExecutionContext = system.dispatcher
+  implicit val ec: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
 
+  logger.info("Opening websocket connecting ...")
 
-      logger.info("Opening websocket connecting ...")
+  val sharedKillSwitch = KillSwitches.shared("my-kill-switch")
 
-      val sharedKillSwitch = KillSwitches.shared("my-kill-switch")
-
-      val testSource =
-        Source
-          .repeat("Hello")
-          .throttle(1, 1.seconds)
-          .map(TextMessage.Strict)
-          .limit(1000)
-          .via(sharedKillSwitch.flow)
-            .runWith(Sink.foreach(i => logger.info(i.text)))
+  val testSource =
+    Source
+      .repeat("Hello")
+      .throttle(1, 1.seconds)
+      .map(TextMessage.Strict)
+      .limit(1000)
+      .via(sharedKillSwitch.flow)
+      .runWith(Sink.foreach(i => logger.info(i.text)))
 
 
-      Source
-        .single("kill source 1")
-        .delay(5.second)
-        .runWith(Sink.foreach(i => logger.info(i)))
-        .onComplete{_ =>
-          sharedKillSwitch.shutdown()
-          system.terminate()
-        }
+  Source
+    .single("kill source 1")
+    .delay(5.second)
+    .runWith(Sink.foreach(i => logger.info(i)))
+    .onComplete { _ =>
+      sharedKillSwitch.shutdown()
+      system.terminate()
+    }
 
 }
